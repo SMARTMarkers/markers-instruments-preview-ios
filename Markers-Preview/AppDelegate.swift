1a35b488c82eb38db2833279cda2d604ef94aca1
Raheel Sayeed <raheelsayeed@osxlap04366.tch.harvard.edu>
2019-11-20 20:25:46 -0500

    first commit -app

---
+++ Markers-Preview/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SMARTMarkers Instruments
+//
+//  Created by Raheel Sayeed on 7/25/19.
+//  Copyright © 2019 Boston Children's Hospital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        
+        return true
+    }
+
+    
+
+}
+
---
+++ Markers-Preview/MasterViewController.swift
@@ -0,0 +1,234 @@
+//
+//  MasterViewController.swift
+//  SMARTMarkers Instruments
+//
+//  Created by Raheel Sayeed on 7/25/19.
+//  Copyright © 2019 Boston Children's Hospital. All rights reserved.
+//
+
+import UIKit
+import SMARTMarkers
+import SMART
+import HealthKit
+
+class MasterViewController: UITableViewController {
+
+    var stepActivity: StepActivity?
+    var pdController: TaskController?
+    var sessionController: SessionController?
+    var instruments = [Instrument]()
+    var patient: Patient?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Patient.read("b85d7e00-3690-4e2a-87a0-f3d2dfc908b3", server: Server.Demo()) { (p, e) in
+            self.patient = (p as! Patient)   
+        }
+        
+        tableView.allowsMultipleSelectionDuringEditing = true
+        navigationItem.leftBarButtonItem = editButtonItem
+
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "All", style: .plain, target: self, action: #selector(beginAll(_:))),
+            UIBarButtonItem(title: "Selected", style: .plain, target: self, action: #selector(beginSelected(_:)))
+        ]
+        
+        
+        
+        
+        if let bmi = localQuestionnaire("promis_q_dynamic") {
+            instruments.append(bmi)
+        }
+        
+        if let bmi = localQuestionnaire("bmi_r4") {
+            instruments.append(bmi)
+        }
+        if let bmi = localQuestionnaire("dynamicquestionnaire") {
+            instruments.append(bmi)
+        }
+        if let bmi = localQuestionnaire("dynamicquestionnaire_valuecoding") {
+            instruments.append(bmi)
+        }
+        
+        let settings = [
+            "client_id": "smpro-api",
+            "client_secret": "0620d1c5-a38e-4ac3-8ed2-d3a3be82add2",
+            "scope": "bloodpressure activity openid offline_access",
+            "redirect_uris": ["smpro://"],
+            "authorize_uri": "https://ohi-oauth.numerasocial.com/connect/authorize",
+            "token_uri": "https://ohi-oauth.numerasocial.com/connect/token",
+            "keychain" : true
+            ] as [String : Any]
+        
+        
+        
+        instruments.append(contentsOf: SMARTMarkers.Instruments.ActiveTasks.allCases.map { $0.instance })
+        instruments.append(contentsOf: SMARTMarkers.Instruments.HealthKit.allCases.map { $0.instance })
+
+        tableView.reloadData()
+    }
+    
+    
+    @objc func beginSelected(_ sender: Any?) {
+        
+        guard let selected = tableView.indexPathForSelectedRow else {
+            return
+        }
+        
+        let instrument = instruments[selected.row]
+        self.pdController = TaskController(instrument: instrument)
+        startMeasure(measure: self.pdController!)
+        
+    }
+
+    @objc
+    func beginAll(_ sender: Any?) {
+        
+        let _store = HKHealthStore()
+        let start  = Date.yesterday.dayBefore.dayBefore.dayBefore
+        let end    = Date.tomorrow
+        
+        
+        print(start)
+        print(end)
+        
+        stepActivity = StepActivity(start, end)
+            
+        stepActivity?.fetch(_store, callback: { (samples, error) in
+            print(samples)
+        })
+        
+        
+        
+    }
+
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return instruments.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.accessoryType = .detailDisclosureButton
+        cell.editingAccessoryType = .detailDisclosureButton
+        let instrument = instruments[indexPath.row]
+        cell.textLabel!.text = instrument.sm_title
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let instrument = instruments[indexPath.row]
+        
+        pdController = TaskController(instrument: instrument)
+        
+        pdController?.reports(for: self.patient!, server: Server.Demo(), callback: { [unowned self] (success, error) in
+            
+            self.pdController?.reports?.reports.forEach({ (resource) in
+                print("\(resource.id?.string ?? ""): \(resource.sm_resourceType())")
+            })
+        })
+        
+        pdController?.createRequest(on: Server.Demo(), for: patient!, from: nil, callback: { (r, e) in
+            print(r?.rq_identifier)
+            print(e)
+        })
+    }
+    
+    func startMeasure(measure: TaskController) {
+        
+        pdController?.prepareSession(callback: { (task, error) in
+            if let controller = task {
+                self.present(controller, animated: true, completion: nil)
+            }
+        })
+        pdController?.onTaskCompletion = { [unowned self] submissionBundle, error in
+            
+            submissionBundle?.canSubmit = true
+            
+            if let reports = self.pdController?.reports {
+                reports.submit(to: Server.Demo(), patient: self.patient!, callback: { (success, error) in
+                    print("submission")
+                    print("==================")
+                    print(success)
+                    print(error)
+                    print(reports.reports)
+                })
+            }
+        }
+    }
+    
+
+    func startSession(for measures: [TaskController]) {
+        
+
+        sessionController = SessionController(measures, patient: patient, server: Server.Demo(), verifyUser: false)
+        
+        sessionController?.prepareController(callback: { (controller, error) in
+            if let controller = controller {
+                self.present(controller, animated: true, completion: nil)
+            }
+        })
+        
+        sessionController?.onConclusion = { session in
+            print(session.identifier)
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+        let instrument = instruments[indexPath.row]
+        let viewController = InstrumentViewController(instrument)
+        show(viewController, sender: nil)
+    }
+    
+    
+
+    
+    
+    
+    
+    func localQuestionnaire(_ filename: String) -> Questionnaire? {
+        
+        let bundle = Bundle(identifier: "org.chip.SMARTMarkers")
+        if let filePath = bundle!.path(forResource: filename, ofType: "json"),
+            let data = NSData(contentsOfFile: filePath) {
+            do {
+                
+                // here you have your json parsed as string:
+                //let jsonString = try? String(contentsOfFile: filePath, encoding: String.Encoding.utf8)
+                
+                // but it is better to use the type data instead:
+                let jsonData = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments)
+                let q = try Questionnaire(json: jsonData as! FHIRJSON)
+                return q
+            }
+            catch {
+                print(error)
+            }
+            
+        }
+        return nil
+    }
+
+
+}
+
+
+
+extension MasterViewController: InstrumentResolver {
+    
+    func resolveInstrument(in controller: TaskController) -> Instrument? {
+        return nil
+    }
+    
+}
